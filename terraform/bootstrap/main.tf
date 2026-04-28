d68e2cf11a7f950fe9674a98656d5769025fa70d
sirshendud076 <sirshendud076@gmail.com>
2026-04-28 11:45:09 -0600

    MTY Devops training

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,10 +1,4 @@
-module "ecr" {
-  source              = "../modules/ecr"
-  project_name        = var.project_name
-  frontend_repo_name  = var.frontend_repo_name
-  backend_repo_name   = var.backend_repo_name
-  lifecycle_keep_last = var.lifecycle_keep_last
-}
+
 
 module "ssm" {
   source             = "../modules/ssm"
